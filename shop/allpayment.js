5b43468763fdcf1774844b6ee0f518cff9eb9323
JouganBot <kakcikmhdalwi@gmail.com>
2021-11-03 00:49:48 +0700

    Safira cantik

---
+++ shop/allpayment.js
@@ -0,0 +1,31 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ITSMEALWI PERLU DI INGAT INI HANYA PERCOBAAN GA DI JUAL BENERAN
+
+*さ BANK*
+1. BRI 
+	ㅁ 5074-01-003141-52-86
+		• R***A*DI
+		
+*さ E-MONEY*
+1. GOPAY
+	ㅁ 0887 4350 473267
+		• ZE**** **C
+2. OVO
+	ㅁ 0887 4350 473262
+		• EL*****TI
+3. DANA
+	ㅁ 0887 4350 473263
+		• ZE**** **C
+
+*さ PULSA*
+1. SMARTFREN
+	ㅁ 0887 4350 473266
+2. TELKOMSEL
+	ㅁ 0821 9311 57235
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
